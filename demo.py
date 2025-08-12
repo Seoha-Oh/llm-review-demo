5c193aad32530e095290ffe8031f443974bc48b8
SeohaOh <53349284+Seoha-Oh@users.noreply.github.com>
2025-08-12 15:04:32 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,10 @@
 cache = {}
 
-def bad_default(values=[]):  # mutable default (Precondition)
+def bad_default(values=[]):  
     values.append(1)
     return values
 
 def may_raise(d):
-    # Runtime: KeyError 가능성, 파일 누수
     f = open("data.txt", "w")
     f.write(str(d["x"]))
-    # f.close() 빠짐
     return True
